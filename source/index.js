5be0743893b35c9e5126a9cd916eb64ec975556b
artem@dobroagency.ru <cs1035idIII>
2018-08-05 22:32:40 +0400

    ready desktop

--- source/index.js
+++ source/index.js
@@ -1,1 +1,11 @@
 import './style/main.scss';
+
+const headerItems = document.querySelectorAll('.header__item');
+
+headerItems.forEach((item)=>{
+  item.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let rel = document.querySelector(`.${e.target.getAttribute('data-rel')}`);
+    rel.scrollIntoView({behavior: 'smooth'});
+  });
+});
